0207c0fa78a4b93fe4b314e482454981c3de82e0
dorrito <christowrites@gmail.com>
2026-05-29 15:54:20 +0530

    Created controllers, models folder

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require("express")
-
+const mongoose = require("mongoose")
 const app = express()
+
+//Paste your mongo_url from website in "" (remove the process.env.MONGODB_CONNECTION_URL)
+const mongo_url = process.env.MONGODB_CONNECTION_STRING
+
+app.post("/",(req,res)=>{
+    res.send("Hello World Aaditya")
+})
 
 app.listen(8080, ()=>{
     console.log("Server is listenting on port 8080")
