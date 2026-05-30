022cfd6760eff887c489b5bb70766f47731c511c
dorrito <christowrites@gmail.com>
2026-05-30 10:00:50 +0530

    Dns fix for mongodb

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,17 @@ const app = express()
 
 //Paste your mongo_url from website in "" (remove the process.env.MONGODB_CONNECTION_URL)
 const mongo_url = process.env.MONGO_CONNECTION_STRING
+//DNS Fix for Mongodb
+const dns = require("dns")
+dns.setServers(["8.8.8.8","8.8.4.4"])
+
 
 mongoose.connect(mongo_url)
 .then( ()=>{console.log("MongoDB instance connected successfully")})
 .catch((err)=> console.log("Unable to connect to Mongodb"))
 
 app.post("/",(req,res)=>{
-    res.send("Hello World Aaditya")
+    res.send("Hello World")
 })
 
 app.listen(8080, ()=>{
