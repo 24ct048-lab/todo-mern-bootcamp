4e4ec7c3752a2a4adce1a5bd9a94ee3d93ed04e6
dorrito <christowrites@gmail.com>
2026-05-30 15:52:07 +0530

    Incompelete: delete push

--- frontend/src/Component/Tasks.jsx
+++ frontend/src/Component/Tasks.jsx
@@ -2,19 +2,20 @@ import {useState,useEffect} from 'react'
 import {Card} from './Card'
 import {Input} from './Input'
 import axios from 'axios'
-const initialTasks = [
-        {id:0, text: "ONE"},
-        {id:1, text:"Two"},
-        {id:2, text:"Three"},
-    ]
+// const initialTasks = [
+//         {id:0, text: "ONE"},
+//         {id:1, text:"Two"},
+//         {id:2, text:"Three"},
+//     ]
 
 export const Tasks = ()=>{
-    const [ tasks,setTasks] = useState(initialTasks)
+    const [ tasks,setTasks] = useState([])
     
     const fetchTasks = async ()=>{
         try{
             const response = await axios.get("http://localhost:8080/")
             console.log(response)
+            setTasks(response.data)
         }catch(error){
             console.log(error)
         }
@@ -22,29 +23,33 @@ export const Tasks = ()=>{
 
     useEffect(()=>{fetchTasks()},[])
 
-    const addTaskHandler = (newTask)=>{
-        setTasks((prev)=>{
-            return [
-                ...prev,
-                {
-                    id:Math.random(),
-                    text: newTask
-                }
-            ]
-        })
+    const addTaskHandler = async (newTask)=>{
+        try{
+            const body = {
+                "activity_id":Math.random(),
+                "todo_description":newTask
+            }
+            await axios.post("http://localhost:8080/create",body)
+            await fetchTasks()
+        }catch(error){
+            console.log(error)
+        }
     }
 
-    const deleteTaskHandler = (completedId)=>{
-        setTasks((prev)=>{
-            return prev.filter((task)=>task.id !== completedId)
-        })
+    const deleteTaskHandler = async (completedId)=>{
+        try{
+             await axios.delete("http://localhost:8080/{completedId}")
+             await fetchTasks()
+        }catch(error){
+            console.log(error)
+        }
     }
     return (
         <div className='tasks'>
             <h1>Tasks</h1>
             <Input onAddTask = {addTaskHandler}/>
             {tasks.map((task)=>{
-                return <Card key={task.id} id={task.id} text={task.text} onTaskComplete={deleteTaskHandler}/>
+                return <Card key={task.activity_id} id={task.activity_id} text={task.todo_description} onTaskComplete={deleteTaskHandler}/>
             })}
                
         </div>
