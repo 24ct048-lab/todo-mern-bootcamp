a50af1cf1308efbd904783123843dfc5a150b7f2
dorrito <christowrites@gmail.com>
2026-05-30 11:43:23 +0530

    Create api completed

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const { createActivity } = require("./controllers/activityController")
 
 const app = express()
 
 //DNS Fix for Mongodb
 const dns = require("dns")
-dns.setServers(["8.8.8.8","8.8.4.4"])
+dns.setServers(["8.8.8.8", "8.8.4.4"])
 
 //Paste your mongo_url from website in "" (remove the process.env.MONGODB_CONNECTION_URL)
 const mongo_url = process.env.MONGO_CONNECTION_STRING
 mongoose.connect(mongo_url)
-.then( ()=>{console.log("MongoDB instance connected successfully")})
-.catch((err)=> console.log("Unable to connect to Mongodb"))
+  .then(() => { console.log("MongoDB instance connected successfully") })
+  .catch((err) => console.log("Unable to connect to Mongodb"))
 
 
-app.post("/create",)
+app.post("/create", createActivity)
 
-app.listen(8080, ()=>{
-    console.log("Server is listenting on port 8080")
+app.listen(8080, () => {
+  console.log("Server is listenting on port 8080")
 })
 
